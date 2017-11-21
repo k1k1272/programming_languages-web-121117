356e8a21396caf84c7764c059866b81ba1e81a1b
Kenny Hom <kenny.hom27@gmail.com>
2017-11-21 03:52:16 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   languages.each do |type, lang|
     lang.map do |k, v|
       new_hash[k] ||= v
-      new_hash[k][:style] = []
+      new_hash[k][:style] ||= []
       new_hash[k][:style] << type
     end
   end
